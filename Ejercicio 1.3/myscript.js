95a76b8a996b5b599a7b536999dfb630c73005e9
vanessafernandezr <vanefernandezr@gmail.com>
2021-01-29 19:11:13 +0100

    Ejercicio 1.3

---
+++ Ejercicio 1.3/myscript.js
@@ -0,0 +1,44 @@
+////////////////////////////////////// Forma número 1:
+
+const data = ['hola', 2, 5, 'adios'];
+
+// Determina cuál de los dos números que hay en el array es mayor.
+
+console.log ("'5' es mayor que '2'", data[2]>data[1]);
+
+// Operaciones numéricas
+
+console.log ('Suma', data[1]+data[2]);
+console.log ('Resta', data[1]-data[2]);
+console.log ('Multiplicación', data[1]*data[2]);
+console.log ('División', data[1]/data[2]);
+console.log ('Cociente', data[1]%data[2]);
+
+////////////////////////////////////// Forma número 2:
+
+var var1 = 'hola', var2 = 2, var3 = 5, var4 = 'adios'
+
+// Determina cuál de los dos números que hay en el array es mayor.
+
+mayor = var3>var2;
+console.log (mayor + '. El número 5 es mayor que 2');
+
+// Operaciones numéricas
+
+sum = var2 + var3;
+console.log ('La suma es: '+ sum);
+
+rest = var2 - var3;
+console.log ('La resta es: '+ rest);
+
+mul = var2 * var3;
+console.log ('La multiplicación es: '+ mul);
+
+div = var2 / var3;
+console.log ('La división es: '+ div);
+
+coc = var2 % var3;
+console.log ('El cociente es: '+ coc);
+
+
+
