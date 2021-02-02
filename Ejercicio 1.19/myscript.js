577c3edb7dc9962a82a28ea88cfbb70c47ca659d
vanessafernandezr <vanefernandezr@gmail.com>
2021-02-02 17:15:37 +0100

    ejercicio 1.19

--- Ejercicio 1.19/myscript.js
+++ Ejercicio 1.19/myscript.js
@@ -4,28 +4,36 @@ function save () {
     alert ('Se han guardado los datos')
 };
 
-/* Cuando hagamos foco sobre el input del nombre el fondo será de un color y cuando
-se lo quitemos, de otro */
+/* Cuando hagamos foco sobre el input del nombre el fondo será amarillo y cuando
+    se lo quitemos, de otro.
+
+    La variable inputName es la que captura el nombre que se escriba en la casilla.
+
+    Se ha definido el onFocus (changeColorFocus) el color lila y en el onFocusOut (changeColorNoFocus) el rosa*/
 
 const inputName = document.querySelector(".name");
   
 function changeColorFocus() {
-    inputName.style.backgroundColor = '#fff666';
+    inputName.style.backgroundColor = '#D2B4DE';
 };
   
 function changeColorNoFocus() {
-    inputName.style.backgroundColor = '#EC7063';
+    inputName.style.backgroundColor = '#F5B7B1';
 };
   
 
+/* Se define la variable que captura la tecla pulsada como inputLetter
+   La propiedad which devuelve el código de carácter Unicode de la tecla que activó el evento onkeypress
+   Los números 97, 101, 105, 111, 117 definen las vocales
+   Amarillo para las vocales, azul para consonantes */
 
-const inputRandom = document.querySelector('.random');
+const inputLetter = document.querySelector('.random');
   
 function checkLetter(event){
-    const currentLetter = event.which;
-    if (currentLetter === 97 || currentLetter === 101 || currentLetter === 111 || currentLetter === 117) {
-        inputRandom.style.color = '#7B241C';
+    const letter = event.which;
+    if (letter === 97 || letter === 101 || letter === 105 || letter === 111 || letter === 117) {
+        inputLetter.style.color = '#F4D03F' ;
     } else {
-        inputRandom.style.color = '#196F3D';
+        inputLetter.style.color = '#3498DB';
     }
 };
